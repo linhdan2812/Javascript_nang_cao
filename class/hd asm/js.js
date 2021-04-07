1f6591b384f2b958edf6e0bd1f9949f1673d711c
linhdan2812 <linhdanzyl2812@gmail.com>
2021-04-07 19:29:58 +0700

    thêm thư mục database

--- class/hd asm/js.js
+++ class/hd asm/js.js
@@ -1,13 +1,13 @@
 var urlProducts = 'http://localhost:3000/products';
+// lấy dữ liệu
 fetch(urlProducts)
     .then(res => res.json())
     .then(data => {
-        //console.log(data);
         data.forEach(product => {
             renderPro(product);
         });
     });
-// hiẻn thị dữ liệu
+// hiển thị dữ liệu
 var tabProduct = document.querySelector("#tableProducts");
 var renderPro = product => {
     output = `
@@ -16,10 +16,10 @@ var renderPro = product => {
         <td>${product.name}</td>
         <td>${product.price}</td>
         <td>
-            <a class="btnEdit btn btn-primary btn-sm">Sửa</a> |
+            <a class="btnEdit btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModalCenter1">Sửa</a> |
             <a class="btnDel btn btn-danger btn-sm">Xóa</a>
         </td>
-</tr>`;
+    </tr>`;
     tabProduct.insertAdjacentHTML('beforeend', output);
     // xoá dữ liệu
     var btnDel = document.querySelector(`[data-id = '${product.id}'] .btnDel`);
@@ -33,4 +33,39 @@ var renderPro = product => {
                 .then(() => location.reload());
         }
     });
-}
+    // sửa put
+    var edit = document.querySelector(".form-edit");
+    var btnEdit = document.querySelector(`[data-id = '${product.id}'] .btnEdit`);
+    btnEdit.addEventListener('click', e => {
+        e.preventDefault();
+        alert(product.id + "+" + product.name);
+    })
+    // edit.addEventListener('submit', e => {
+    //     e.preventDefault();
+
+    // })
+};
+// thêm sp
+var add = document.querySelector(".form-user");
+add.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(urlProducts, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                id: add.id.value,
+                name: add.name.value,
+                price: add.price.value
+            }
+        )
+    })
+        .then(res => res.json())
+        .then(data => {
+            var arr = [];
+            arr.push(data);
+            renderPro(arr)
+        });
+})
