686245bead1f427d3bc2ce57a837b30dab7c000c
linhdan2812 <linhdanzyl2812@gmail.com>
2021-04-12 13:43:29 +0700

    assignment

--- class/ASS/admin/js/index.js
+++ class/ASS/admin/js/index.js
@@ -1,2 +1,29 @@
 var urlCate = ' http://localhost:3000/cate';
 var urlProducts = 'http://localhost:3000/products';
+var urlCust = 'http://localhost:3000/customer';
+var countCate = document.querySelector(".countcate");
+var countProd = document.querySelector(".countProd");
+var countCust = document.querySelector(".countCust");
+fetch(urlCate, {
+    method: "GET"
+})
+    .then(res => res.json())
+    .then(cateData => {
+        countCate.innerHTML = cateData.length;
+    })
+
+fetch(urlProducts, {
+    method: "GET"
+})
+    .then(res => res.json())
+    .then(proData => {
+        countProd.innerHTML = proData.length;
+    })
+
+fetch(urlCust, {
+    method: "GET"
+})
+    .then(res => res.json())
+    .then(custData => {
+        countCust.innerHTML = custData.length;
+    })
